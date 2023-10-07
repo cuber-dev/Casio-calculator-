d5771eb5a14281c021cc8bcc2b21752231727c96
adepunandhu9@gmail.com <adepunandhu9@gmail.com>
2023-10-07 11:55:49 +0530

    fixed some bugs

--- main.js
+++ main.js
@@ -22,11 +22,11 @@ buttons.forEach((button) => {
     else{
       if(display.innerText === "0" || display.innerText === "Invalid expression"){
         display.innerText = button.value;
+      }else if (display.innerText.slice(-1).includes(button.value)) {
+        return;
       }else if(/[+\/*\-%]$/.test(display.innerText) && /[+\/*\-%]/.test(button.value)){
         display.innerText = display.innerText.slice(0, -1) + button.value;
-      } else if(display.innerText.includes('.') && button.value === '.'){
-        return;
-      }else{
+      } else{
         display.innerText += button.value;
       }
     }
